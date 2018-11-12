381ee5a01faca53a320add6dcc400412fe8c1f4a
shwapneel <s2ishraq@edu.uwaterloo.ca>
2018-11-12 08:09:56 -0500

    The JSON validation function works, as well as the update channel now

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 var app = require ('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
-/*var server = app.listen(3000, function(){
-   console.log('test123');
-})*/
-
+//send the index.html file to all clients
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
+//socket.io creates a connection
 io.on('connection',function(socket){
-    //console.log('a user is connected');
-    socket.on('disconnect', function(){
-      console.console.log('user disconnected');
+    //when we get a signal that the input JSON command is correct
+    socket.on('JSON_INSERT',function(data){
+       //send the JSON command to all clients in the Update channel
+       io.sockets.emit('JSON_INSERT',data);
+       //add the JSON code to the end of the file
+      // fs.appendFile('JSON.txt', data )
+    });
+
+    socket.on('KEY_FIND',function(data){
+       //send the value at the key to all clients in the Update channel
+       io.sockets.emit('KEY_FIND',data);
+
     });
 });
 
+//use localhost:3420 for this porgram
 http.listen(3420,function(){
     console.log('Listening on *3420');
 });
